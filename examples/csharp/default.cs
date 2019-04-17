df83bbd3eb28abb51de6c6c47e21bfd393292a24
Dan Shechter <dans@houmus.org>
2019-04-17 14:32:10 +0300

    mmm

---
+++ examples/csharp/default.cs
@@ -0,0 +1,14 @@
+// Type your code here, or load an example.
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
